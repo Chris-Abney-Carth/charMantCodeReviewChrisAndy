518384660008dfa814f166f1eaf422f7c4e66f4b
Andy Del Real <afdelreal04@gmail.com>
2025-03-23 23:24:06 -0500

    working add function along with a few helper functions

--- main.cpp
+++ main.cpp
@@ -12,8 +12,10 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 
-int power(int base, int exp);
-int getMagnitude(int number);
+int numDigits(int number);
+void addNumbers(int c1, int n1, int d1, int c2, int n2, int d2, int& newCharacterisitc, int& newNumerator, int& newDenominator);
+void appendCharacteristic(char result[], int characteristic, int numOfDigits, int& charsUsed, int len);
+void appendMantissa(char result[], int newNumerator, int newDenominator, int charsUsed, int len);
 
 int main()
 {
@@ -42,13 +44,13 @@ int main()
     int c2, n2, d2;
 
     //initialize the values
-    c1 = 1;
-    n1 = 1;
-    d1 = 2;
+    c1 = 10; // change this back to 1
+    n1 = 5;
+    d1 = 10;
 
-    c2 = 2;
-    n2 = 2;
-    d2 = 3; 
+    c2 = -2;
+    n2 = 834;
+    d2 = 1000; 
 
     //if the c-string can hold at least the characteristic
     if(add(c1, n1, d1, c2, n2, d2, answer, 10))
@@ -93,49 +95,43 @@ bool mantissa(const char numString[], int& numerator, int& denominator)
 //--
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    //you will have to come up with an algorithm to add the two numbers
-    //hard coded return value to make the main() work
-    // result[0] = '4';
-    // result[1] = '.';
-    // result[2] = '1';
-    // result[3] = '6';
-    // result[4] = '6';
-    // result[5] = '6';
-    // result[6] = '6';
-    // result[7] = '6';
-    // result[8] = '6';
-    // result[9] = '\0';
-    if (len > 10 || len <= 0)
+    // if len is at most 10 (9 characters and null terminator), but at least 2 (for a digit and null terminator)
+    if (len > 10 || len <= 1)
     {
         return false;
     }
 
-    int added_characteristic = c1 + c2;
+    int addedCharacteristic = 0;
+    int newNumerator = 0;
+    int newDenominator = 0;
 
-    if (abs(added_characteristic) >= power(10, len))  // change this to accout for mantissa with addition
+    addNumbers(c1, n1, d1, c2, n2, d2, addedCharacteristic, newNumerator, newDenominator); // get the added values
+    int numOfDigits = numDigits(addedCharacteristic); 
+    if (addedCharacteristic < 0)
     {
-        return false;  // we can't store the added characterisitic
+        numOfDigits++;
+    }
+
+    if (numOfDigits >= len)  // check to see if we can hold at least the characteristic
+    {
+        return false;  // we can't store the added chara cterisitic
     }
     else  // if we get here, we can store at least the characteristic
-    {   // update char array to hold characteristic
-        // add into array
-        // get a magnitude
-        // w
-        int magnitude = getMagnitude(added_characteristic);
+    {  
+        int charsUsed = 0;
 
-        if (magnitude >= len - 1) // if our added_characterisitc contains more digits than len - 1
+        appendCharacteristic(result, addedCharacteristic, numOfDigits, charsUsed, len);
+       
+        if (charsUsed >= len - 2) // we cant add anything else 
         {
-            return false;
+            result[charsUsed] = '\0';
+            return true;
+        } 
+        else 
+        {
+            // add everything after decimal
+            appendMantissa(result, newNumerator, newDenominator, charsUsed, len);
         }
-        // for (int i = 0; i < magnitude; i++)
-        // {
-
-        // }
-        // cout << getMagnitude(9) << endl;
-        // cout << getMagnitude(99) << endl;
-        // cout << getMagnitude(100) << endl;
-        // cout << getMagnitude(101) << endl;
-        // cout << getMagnitude(-100) << endl;
     }
  
     return true;
@@ -170,26 +166,7 @@ bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int l
     return true;
 }
 
-int power(int base, int exp)
-{
-    int retVal = base;
-
-    if (power <= 0) 
-    {
-        retVal = 0;
-    }
-    else 
-    {
-        for (int i = 0; i < exp - 1; i++) 
-        {
-            retVal *= base;
-        }
-    }
-
-    return retVal;
-}
-
-int getMagnitude(int number)
+int numDigits(int number)
 {
     number = abs(number);
     int comparissonValue = 10;
@@ -201,5 +178,84 @@ int getMagnitude(int number)
         magnitude++;
     }
 
-    return magnitude;
+    return magnitude + 1;
+}
+
+void addNumbers(int c1, int n1, int d1, int c2, int n2, int d2, int& newCharacteristic, int& newNumerator, int& newDenominator)
+{
+    newDenominator = max(d1, d2);
+
+    if (c1 < 0)
+    {
+        n1 *= -1;
+    }
+
+    if (c2 < 0)
+    {
+        n2 *= -1;
+    }
+
+    newNumerator = n1 * (newDenominator / d1) + n2 * (newDenominator / d2);
+    newCharacteristic = c1 + c2;
+
+    if ((c1 < 0 && c2 > 0) || (c1 > 0 && c2 < 0)) {
+        // If the whole numbers have opposite signs, we need to handle carryover carefully
+        // First, handle the case when the fraction's numerator is negative
+        if (newNumerator > 0 && c1 < 0) {
+            newCharacteristic++;  // Subtract from the whole part
+            newNumerator = newDenominator - newNumerator;  // Add the denominator to the numerator to fix negative fraction
+        }
+        else if (newNumerator < 0 && c2 < 0){
+            newCharacteristic--;
+            newNumerator = newDenominator + newNumerator;
+        }
+    } 
+    else {
+        // If the whole numbers are the same sign, add the whole number carryover directly from the fraction
+        newCharacteristic += newNumerator / newDenominator;
+        newNumerator %= newDenominator;  // Fraction remainder
+        newNumerator = abs(newNumerator);
+    }
+}
+
+// maybe make append characteristic
+void appendCharacteristic(char result[], int characteristic, int numOfDigits, int & charsUsed, int len)
+{
+    if (characteristic < 0)
+    {
+        result[0] = '-';
+        charsUsed++;
+    }
+
+    int stop = (characteristic < 0 ? 1 : 0);
+    characteristic = abs(characteristic);
+    
+    for (int i = numOfDigits - 1; i >= stop; i--)
+    {
+        result[i] = '0' + (characteristic % 10);
+        characteristic /= 10;
+        charsUsed++;
+    }
+}
+
+void appendMantissa(char result[], int newNumerator, int newDenominator, int charsUsed, int len) 
+{
+    int digit = 0;
+    result[charsUsed++] = '.';
+
+    cout << newNumerator << endl;
+    cout << newDenominator << endl;
+    while (newDenominator != 1) 
+    {
+        newDenominator /= 10;
+        digit = newNumerator / newDenominator;
+        newNumerator -= digit * newDenominator;
+        result[charsUsed++] = '0' + digit;
+    }
+
+    for (int i = charsUsed; i < len - 1; i++)
+    {
+        result[i] = '0';
+    }
+    result[len - 1] = '\0';
 }
